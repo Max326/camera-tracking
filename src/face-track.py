be8fc0bd0bfdccd2e4615cde4ce7a4f24b97981d
Max326 <maxconnor326@gmail.com>
2025-02-28 23:08:42 +0100

    trying to send to servo directly

--- src/face-track.py
+++ src/face-track.py
@@ -1,6 +1,17 @@
 import cv2
 from gpiozero import AngularServo
 from time import sleep
+import serial
+
+ser = serial.Serial('/dev/serial0', baudrate=115200, timeout=1)
+
+def sendAngle(angle):
+    if 100 <= angle <= 140:
+        msg = f"{angle}\n"
+        ser.write(msg.encode())
+        print(f"sent: {msg.strip()}")
+    else:
+        print("error: angle out of range")
 
 # Initialize the servo
 servo = AngularServo(18, min_pulse_width=0.0005, max_pulse_width=0.0023)
@@ -34,23 +45,26 @@ while True:
         (x, y, w, h) = faces[0]
 
         # Calculate the center of the face
-        face_center_x = x + w // 2
+        face_center_y = y + w // 2
 
         # Get the width of the frame
         frame_width = frame.shape[1]
 
         # Map the face position to servo range [-45, 45]
-        normalized_position = (face_center_x - frame_width / 2) / (frame_width / 2)
+        normalized_position = (face_center_y - frame_width / 2) / (frame_width / 2)
         target_angle = -60 * normalized_position  # Map to [-45, 45]
 
         # Limit the angle within the servo range
         target_angle = max(-60, min(60, target_angle))
 
+        sendAngle(target_angle)
+        current_angle = target_angle
+
         # Move the servo if the angle difference is significant
-        if abs(target_angle - current_angle) > 1:  # Update only if change > 1 degree
-            servo.angle = target_angle
-            current_angle = target_angle
-            print(f"Servo moved to: {current_angle:.2f} degrees")
+        # if abs(target_angle - current_angle) > 1:  # Update only if change > 1 degree
+        # servo.angle = target_angle
+        # current_angle = target_angle
+        print(f"Servo moved to: {current_angle:.2f} degrees")
 
     # Display the video feed
     cv2.imshow('Face Detection', frame)
--- src/servo.py
+++ src/servo.py
@@ -1,7 +1,7 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(18, min_pulse_width=0.0005, max_pulse_width=0.0025)
+servo = AngularServo(18, min_pulse_width=0.0005, max_pulse_width=0.0023)
 
 while True:
     servo.angle = -45
